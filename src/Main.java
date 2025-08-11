f3eac3d38d3d0e26c296cc56ac1233a070d92f34
Juan Cruz Scolari <juanscolari13@gmail.com>
2025-08-11 10:17:13 -0300

    nada

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-public class Main1{
+public class Main{
     public static void main(String[]arg){
         System.out.println("chau");
         System.out.println("Estamos en la rama login");
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hola");
         }
     }
 }
