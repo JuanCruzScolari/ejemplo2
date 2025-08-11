88629ff63fffc4c05c34a5eb867a05da7df7ec01
Juan Cruz Scolari <juanscolari13@gmail.com>
2025-08-11 10:03:33 -0300

    puse un sout y un bucle for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class main{
     public static void main(String[]arg){
-        System.out.println("Hola");
+        System.out.println("chau");
+        System.out.println("Estamos en la rama login");
+        for (int i = 0; i < ; i++) {
+            
+        }
     }
 }
